e4d55a16a3c04ff69f6881907a0b61672bbc1cf3
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2023-11-23 17:54:20 +0300

    23-11-2023-17-54

---
+++ DapperORM.Application/Exceptions/UserCreatedFailedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperORM.Application.Exceptions
+{
+    public class UserCreatedFailedException:Exception
+    {
+        public UserCreatedFailedException():base("Kullanıcı kayıt edilirken beklenmeyen hata alındı!")
+        {
+
+        }
+        public UserCreatedFailedException(string? message) :base(message)
+        {
+            
+        }
+        public UserCreatedFailedException(string? message,Exception? innerException) :base(message,innerException)
+        {
+            
+        }
+    }
+}
--- DapperORM.Domain/Common/BaseEntity.cs
+++ DapperORM.Domain/Common/BaseEntity.cs
@@ -9,5 +9,7 @@ namespace DapperORM.Domain.Common
     public class BaseEntity
     {
         public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        virtual public DateTime UpdatedDate { get; set; }
     }
 }

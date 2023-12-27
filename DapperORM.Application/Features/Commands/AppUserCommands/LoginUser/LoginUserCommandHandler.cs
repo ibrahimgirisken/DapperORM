49f84f7a9b17822761f9607942612d759d7e7fbe
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2023-12-27 12:12:18 +0300

    27-12-2023-12-12

--- DapperORM.Application/Features/Commands/AppUserCommands/LoginUser/LoginUserCommandHandler.cs
+++ DapperORM.Application/Features/Commands/AppUserCommands/LoginUser/LoginUserCommandHandler.cs
@@ -2,41 +2,36 @@
 using DapperORM.Application.DTOs;
 using DapperORM.Application.Exceptions;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 namespace DapperORM.Application.Features.Commands.AppUserCommands.LoginUser
 {
     public class LoginUserCommandHandler:IRequestHandler<LoginUserCommandRequest,LoginUserCommandResponse>
     {
-        //readonly AspNetUserManager<IdentityUser> _userManager;
-        //readonly SignInManager<IdentityUser> _signInManager;
-        //readonly ITokenHandler _tokenHandler;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ITokenHandler _tokenHandler;
 
-        //public LoginUserCommandHandler(AspNetUserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ITokenHandler tokenHandler)
-        //{
-        //    _userManager = userManager;
-        //    _signInManager = signInManager;
-        //    _tokenHandler = tokenHandler;
-        //}
 
 
         public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request,CancellationToken cancellationToken)
         {
-            //IdentityUser user = await _userManager.FindByNameAsync(request.UserNameOrEmail);
-            //if (user != null)
-            //    user = await _userManager.FindByEmailAsync(request.UserNameOrEmail);
-            //if (user == null)
-            //    throw new NotFoundUserException("Kullanıcı veya şifre hatalı!");
+            IdentityUser user = await _userManager.FindByNameAsync(request.UserNameOrEmail);
+            if (user != null)
+                user = await _userManager.FindByEmailAsync(request.UserNameOrEmail);
+            if (user == null)
+                throw new NotFoundUserException("Kullanıcı veya şifre hatalı!");
 
-            //SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
-            //if (result.Succeeded)//Authentication başarılı !
-            //{
-            //    //yetkiler belirlenecek!
-            //    Token token = _tokenHandler.CreateAccessToken(5);
-            //    return new LoginUserSuccessCommandResponse()
-            //    {
-            //        Token = token
-            //    };
-            //}
+            SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded)//Authentication başarılı !
+            {
+                //yetkiler belirlenecek!
+                Token token = _tokenHandler.CreateAccessToken(5);
+                return new LoginUserSuccessCommandResponse()
+                {
+                    Token = token
+                };
+            }
             throw new AuthenticationErrorException();
         }
     }

4ac8201e8bb0c5df93c6a6c4f528c3824136c728
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-01-08 09:28:48 +0300

    08-01-2024-09-29

--- DapperORM.Application/Interfaces/Repositories/IGenericRepository.cs
+++ DapperORM.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -12,8 +12,6 @@ namespace DapperORM.Application.Interfaces.Repositories
         T Get(int id);
         T GetByColumnName(string columnName,string columnValue);
         void Add(T entity);
-        void AddRange(IEnumerable<T> entities);
-
         void Update(T entity);
         void Delete(T entity);
         List<T> GetAll();
--- DapperORM.Persistence/Repositories/DapperProductRepository.cs
+++ DapperORM.Persistence/Repositories/DapperProductRepository.cs
@@ -2,8 +2,6 @@
 using DapperORM.Application.Interfaces.DapperContext;
 using DapperORM.Application.Interfaces.Repositories;
 using DapperORM.Domain.Entities;
-using System.Data.Entity;
-using static Dapper.SqlMapper;
 
 namespace DapperORM.Persistence.Repositories
 {
@@ -11,20 +9,6 @@ namespace DapperORM.Persistence.Repositories
     {
         public DapperProductRepository(IDapperContext dapperContext) : base(dapperContext,"Products")
         {
-        }
-
-        public void AddRange(Product products)
-        {
-            var productTranslation = products.Translations.Select(t => new ProductTranslation
-            {
-                Content = t.Content,
-                Title = t.Title,
-                MetaDescription = t.MetaDescription,
-                MetaKeywords = t.MetaKeywords,
-                Name = t.Name,
-                LanguageId = t.LanguageId,
-                ProductId = t.ProductId,
-            });
         }
 
         public List<Product> GetProductByCategoryId(int id)

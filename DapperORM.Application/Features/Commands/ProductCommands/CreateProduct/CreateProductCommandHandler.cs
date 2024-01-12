6f1f0825ff9693cf3c392d58857c0d7c9e98a29b
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-01-12 14:22:07 +0300

    12-01-2024-14-22

--- DapperORM.Application/Features/Commands/ProductCommands/CreateProduct/CreateProductCommandHandler.cs
+++ DapperORM.Application/Features/Commands/ProductCommands/CreateProduct/CreateProductCommandHandler.cs
@@ -14,32 +14,31 @@ namespace DapperORM.Application.Features.Commands.ProductCommands.CreateProduct
 {
     public class CreateProductCommandHandler:IRequestHandler<CreateProductCommandRequest,IDataResult>
     {
-        private readonly IProductRepository _productRepository;
-        private readonly IProductTranslationRepository _productTranslationRepository;
         private readonly IMapper _mapper;
         private readonly CreateProductValidator _createProductValidator;
-        private readonly ITranslationService<Product,ProductTranslation> _translationService;
-        public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper, CreateProductValidator createProductValidator, IProductTranslationRepository productTranslationRepository, ITranslationService<Product, ProductTranslation> translationService)
+        private readonly IProductTranslationService _productTranslationService;
+        public CreateProductCommandHandler(IMapper mapper, CreateProductValidator createProductValidator, IProductTranslationService productTranslationService)
         {
-            _productRepository = productRepository;
-            _productTranslationRepository = productTranslationRepository;
             _mapper = mapper;
             _createProductValidator = createProductValidator;
-            _translationService = translationService;
+            _productTranslationService = productTranslationService;
         }
 
         public  async Task<IDataResult> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
             Product product = _mapper.Map<Product>(request);
             List<ProductTranslation> translations = new();
-
+            var result = _createProductValidator.Validate(product);
+            if (result.Errors.Any())
+            {
              foreach (var item in request.Product.ProductTranslations)
             {
                 ProductTranslation productTranslation =_mapper.Map<ProductTranslation>(item);
                 translations.Add(productTranslation);
             }      
+            }
             
-                _translationService.AddAsync(product, translations);
+               await _productTranslationService.AddAsync(product, translations);
                 return await Task.FromResult<IDataResult>(new SuccessResult(ResultMessages.Product_Added)); 
         }
     }
